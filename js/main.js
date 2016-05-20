59eec97332e9cfd79472c096859eb723a2bf55b2
Vika <viktoriia.mostovaia@mail.ru>
2016-05-20 12:40:32 +0300

    make pop-up

--- js/main.js
+++ js/main.js
@@ -12,4 +12,79 @@ jQuery(document).ready(function() {
             top = jQuery(id).offset().top;
         jQuery('body,html').animate({scrollTop: top}, 700);
     		});
+	jQuery(function($){
+		jQuery('#phone, #popup-phone').mask("+38 (099) 999-99-99",{autoclear: false});
+	});
+
+	jQuery('.apply').click(function(event) {
+				event.preventDefault();
+				jQuery('.blackout').fadeIn(400,
+					function(){
+						jQuery('.pop-up')
+							.css('display', 'block')
+							.animate({opacity: 1, top: '50%'}, 200);
+					});
+			});
+			jQuery('.blackout, .cancel').click(function(){
+				jQuery('.pop-up')
+					.animate({opacity: 0, top: '45%'}, 200,
+						function(){
+							jQuery(this).css('display', 'none');
+							jQuery('.blackout').fadeOut(400);
+
+						});
+			});
+
+
+			(function( $ ){
+	
+//// ---> Проверка на существование элемента на странице
+jQuery.fn.exists = function() {
+   return jQuery(this).length;
+}
+
+  
+  if(jQuery('.footer-form, .popup-form').exists()){
+    
+    var form = $('.footer-form, .popup-form'),
+      btn = form.find('.btn-submit');
+    
+    form.find('.rfield').addClass('empty_field');
+  
+    setInterval(function(){
+    
+      if($('#phone').exists()){
+        var pmc = $('#phone');
+        if ( (pmc.val().indexOf("_") != -1) || pmc.val() == '' ) {
+          pmc.addClass('empty_field');
+        } else {
+            pmc.removeClass('empty_field');
+        }
+      }
+      
+      var sizeEmpty = form.find('.empty_field').size();
+      
+      if(sizeEmpty > 0){
+        if(btn.hasClass('disabled')){
+          return false
+        } else {
+          btn.addClass('disabled')
+        }
+      } else {
+        btn.removeClass('disabled')
+      }
+      
+    },200);
+
+    btn.click(function(){
+      if($(this).hasClass('disabled')){
+        return false
+      } else {
+        form.submit();
+      }
+    });
+    
+  }
+
+});
 });
